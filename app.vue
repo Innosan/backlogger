dcf3e2bb2635d92c0cc32bda15d7dcb2734a5792
Mikhail <90400368+Innosan@users.noreply.github.com>
2024-03-20 17:11:30 +0300

    Initial commit

---
+++ app.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useSettingsStore } from "~/stores/settingsStore";
+
+const settingsStore = useSettingsStore();
+const appConfig = useAppConfig();
+const route = useRoute();
+
+appConfig.ui.primary = settingsStore.primaryColor;
+</script>
+
+<template>
+	<div>
+		<Header />
+		<div class="page-container" v-auto-animate>
+			<NuxtPage :key="route" />
+		</div>
+		<Footer />
+	</div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+export default defineNuxtConfig({
+	devtools: { enabled: true },
+
+	modules: [
+		"@nuxt/ui",
+		"@formkit/auto-animate/nuxt",
+		"@pinia/nuxt",
+		"@pinia-plugin-persistedstate/nuxt",
+	],
+
+	components: [
+		{
+			path: "~/components",
+			pathPrefix: false,
+		},
+	],
+
+	css: ["~/assets/styles/main.css"],
+});
